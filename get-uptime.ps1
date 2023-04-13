cd9a818e83c1e178258a1f0e5d65efec10589363
Andrew <38707579+killer6oose@users.noreply.github.com>
2023-04-12 23:25:17 -0400

    Update get-uptime.ps1

--- get-uptime.ps1
+++ get-uptime.ps1
@@ -1,42 +1,27 @@
-Function Get-Uptime {
-<#
-.Synopsis
-    This will check how long the computer has been running and when was it last rebooted.
- 
- 
-.NOTES
-    Name: Get-Uptime
-    Author: Andrew Hatton
-    Version: 1.4
-    DateCreated: 2018-Jun-16
- 
-.LINK
-    https://cronotech.us/ -
- 
- 
-.PARAMETER ComputerName
-    By default it will check the local computer.
- 
-    Description:
-    Check the computers in selected file for their boottime/uptime.
- 
-#>
- 
-   $serverlist = "Path\To\Server\List\sl.txt" 
-$reporttextfile = "Path\To\Save\Report\uptimereport.txt" 
-out-file -filepath $reporttextfile -append -InputObject ("Uptime Report Run " + (Get-Date)) 
-foreach ($server in Get-Content $serverlist) 
-{ 
- $ping = gwmi win32_pingstatus -f "Address = '$server'" 
- if($ping.statuscode -eq 0) 
- { 
-    $wmi = Get-WmiObject -ComputerName $server -Query "SELECT LastBootUpTime FROM Win32_OperatingSystem"     
-    $now = Get-Date     
-    $boottime = $wmi.ConvertToDateTime($wmi.LastBootUpTime) 
-    out-file -filepath $reporttextfile -append -InputObject ("Uptime for " + $server + ": " + ($now - $boottime)) 
- } 
- else 
- { 
-    out-file -filepath $reporttextfile -append -InputObject ("Uptime for " + $server + ": Host offline") 
- } 
+$serverList = Get-Content -Path "server.txt"
+$outputFile = "output.csv"
+
+# Create the output CSV file and write the header row
+$header = "Hostname, Hours since last reboot, Online"
+$header | Out-File -FilePath $outputFile -Encoding utf8 -Force
+
+foreach ($server in $serverList) {
+    try {
+        # Get the uptime and calculate the hours since last reboot
+        $uptime = Get-Uptime -ComputerName $server
+        $lastReboot = (Get-Date) - $uptime
+        $hoursSinceReboot = [math]::Round((New-TimeSpan -Start $lastReboot).TotalHours, 2)
+
+        # Determine if the server is online
+        $online = Test-Connection -ComputerName $server -Count 1 -Quiet
+
+        # Build the output string and write it to the CSV file
+        $output = "$server, $hoursSinceReboot, $online"
+        $output | Out-File -FilePath $outputFile -Encoding utf8 -Append
+
+    } catch {
+        # If there's an error, write a message indicating that the server is offline
+        $output = "$server, Offline, False"
+        $output | Out-File -FilePath $outputFile -Encoding utf8 -Append
+    }
 }
